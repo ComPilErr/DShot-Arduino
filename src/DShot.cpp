80bf8ab6a2d362208dbb44a363f3b326f040d38b
ComPilErr <compilerr@gmail.com>
2026-06-03 14:05:37 +0300

    Fix Dshot 300 for Bluejay esc
    Signed-off-by: ComPilErr <compilerr@gmail.com>

--- src/DShot.cpp
+++ src/DShot.cpp
@@ -95,7 +95,7 @@ static inline void sendData(){
 		"_sleep_loop_1_2:\n"
 		"DEC	r26\n"			// 1 //
 		"BRNE	_sleep_loop_1_2\n"	// 2 //
-		"NOP\n"				// 1 // BRNE on skip uses only 1 cycle, not 2
+		//// "NOP\n"				// 1 // BRNE on skip uses only 1 cycle, not 2
 		// 1 + 3 * N //
 
 		// Set Low for low bits only
@@ -213,7 +213,7 @@ TCCR1A = 0; // set entire TCCR1A register to 0
 TCCR1B = 0; // same for TCCR1B
 TCNT1  = 0; // initialize counter value to 0
 // set compare match register for 500 Hz increments
-OCR1A = 31999; // = 16000000 / (1 * 500) - 1 (must be <65536)
+OCR1A = 1999; // = 16000000 / (1 * 500) - 1 (must be <65536)
 // turn on CTC mode
 TCCR1B |= (1 << WGM12);
 // Set CS12, CS11 and CS10 bits for 1 prescaler
